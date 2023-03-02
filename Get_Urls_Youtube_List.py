faef81ac05923020d8cdc66341dca003dc5af2fa
GeroZayas <gerozayas@gmail.com>
2023-03-02 09:11:13 +0100

    Update

--- Get_Urls_Youtube_List.py
+++ Get_Urls_Youtube_List.py
@@ -1,3 +1,6 @@
+import re
+
+
 def get_video_urls_from_playlist(the_playlist):
     from pytube import Playlist
 
@@ -7,9 +10,15 @@ def get_video_urls_from_playlist(the_playlist):
     for video in playlist:
         list_of_video_urls.append(video)
 
+    # print(list_of_video_urls)
     return list_of_video_urls
 
 
 if __name__ == "__main__":
+    print(
+        """
+          It must be a public Playlist
+          """
+    )
     a_playlist = input("Insert the playlist link: ")
     get_video_urls_from_playlist(a_playlist)
